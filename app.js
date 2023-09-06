a116d4b6e93cd672bad7da81c02a9df6e00ed5c6
ZmijaZ <zmijaz2001@gmail.com>
2023-09-06 18:07:32 +0200

    add register/login routes

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ connectMongo();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-const { connect } = require("http2");
 
 var app = express();
 
@@ -28,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,68 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 const { body, validationResult } = require("express-validator");
 
-exports.registerUser = asyncHandler(async (req, res, next) => {});
+exports.registerUser = asyncHandler(async (req, res, next) => {
+  const { username, password, confirmPassword } = req.body;
 
-exports.loginUser = asyncHandler(async (req, res, next) => {});
+  console.log(req.body);
+
+  if (!(username && password)) {
+    res.status(400);
+    res.json({ Error: "Please add all fields" });
+  }
+
+  const userExists = await User.findOne({ username });
+  if (userExists) {
+    res.status(400);
+    res.json({ Error: "Username already taken" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username: username,
+    password: hashedPassword,
+  });
+  if (!user) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  } else {
+    res.status(201).json({
+      id: user.id,
+      username: user.username,
+      confirm: req.body.password === req.body.confirmPassword,
+      token: generateToken(user._id),
+    });
+  }
+});
+(exports.loginUser = body("username").trim().escape()),
+  body("password").trim().escape(),
+  asyncHandler(async (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!(password && username)) {
+      res.status(400).send("Invalid username/password");
+    }
+    const user = await User.findOne({ username: username }).exec();
+    if (!user) {
+      res.status(400).send("User not found");
+    }
+
+    const authUser = await bcrypt.compare(password, user.password);
+    if (authUser) {
+      res.status(201).json({
+        id: user._id,
+        username: user.username,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid credentials");
+    }
+  }),
+  function generateToken(id) {
+    return jwt.sign({ id }, process.env.JWT_TOKEN, { expiresIn: "100s" });
+  };
